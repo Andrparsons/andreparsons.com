7bdc3442cb8fce25f6d5056d40ae296aac2dc3b9
andrparsons <andrparsons@gmail.com>
2019-02-25 07:11:54 -0500

    Created toggle function for the  mobile menu and made header responsive

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,44 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
+import Toggle from "../utils/Toggle";
 
 const Header = ({ siteTitle }) => (
   <header className="header">
     <nav className="nav-container">
       <div className="brand">
         <Link to="/">
-          {" "}
           <span className="brand-icon">></span>
           <span className="active">Andre Parsons</span>
         </Link>
       </div>
-      <div className="links">
-        <Link to="about">About</Link>
-        <Link to="about">Coding</Link>
-        <Link to="about">Photograpy</Link>
-        <Link to="about">Blog</Link>
-        <Link to="about">Contact</Link>
-      </div>
+      <Toggle>
+        {({ on, toggle }) => (
+          <>
+            <div
+              className={on ? "menu-btn close" : "menu-btn"}
+              onClick={toggle}
+            >
+              <div class="btn-line" />
+              <div class="btn-line" />
+              <div class="btn-line" />
+            </div>
+            <div
+              className={on ? "overlay active-overlay" : "overlay"}
+              onClick={toggle}
+            />
+            <div className={on ? "links active-links" : "links"}>
+              <Link to="/about">About</Link>
+              <Link to="/coding">Coding</Link>
+              <Link to="/photos">Photograpy</Link>
+              <Link to="/blog">Blog</Link>
+              <Link to="/contact">Contact</Link>
+            </div>
+          </>
+        )}
+      </Toggle>
       <div className="outsite">
-        <Link to="about" className="nav-icon">
+        <Link to="/about" className="nav-icon">
           GH
         </Link>
       </div>
